fce1dfd3d860556fa1a6282d36b2eb93d58a6060
patilprachi509-bit <patilprachi509@gmail.com>
2026-08-06 19:53:02 +0530

    UI: Update background image to homepage-bg

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -51,7 +51,7 @@ export const HomeScreen: React.FC = () => {
       {/* Background Image */}
       <div style={{
         position: 'fixed', top: 0, left: 0, right: 0, bottom: 0,
-        backgroundImage: `url('/screen0.png')`,
+        backgroundImage: `url('/homepage-bg.png')`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         filter: 'blur(2px)',
